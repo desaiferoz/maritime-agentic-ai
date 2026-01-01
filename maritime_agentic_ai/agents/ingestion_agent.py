023ed33906ce8ed1be153073edb9486df4b4b505
desaiferoz <desaiferoz@gmail.com>
2026-01-01 14:36:13 +0530

    Delete maritime_agentic_ai directory

--- maritime_agentic_ai/agents/ingestion_agent.py
+++
@@ -1,8 +0,0 @@
-# agents/ingestion_agent.py
-import pandas as pd
-
-def ingest_data(file_path):
-    df = pd.read_csv(file_path)
-    df.columns = df.columns.str.lower().str.replace(" ", "_")
-    df = df.dropna(subset=["fuel_mass", "shaft_power"])
-    return df
